7124b20e51da5682fc6c297d99f12690d720cd12
Daniel Felix Hrouzek <de.ef.ha@gmail.com>
2017-10-06 00:39:16 +0200

    Příprava na generování JSON (meta)dat

--- scripts/font_test.py
+++ scripts/font_test.py
@@ -3,37 +3,52 @@
 import sys
 sys.path.insert(0, '../libs/')
 
+from objdict import ObjDict
 from klan_font import KlanFont
 
+data = ObjDict()
 font = KlanFont.from_file('../data/sources/00/font.lib')
 
-print "Count: %d" % font.fat.count
+data.header = ObjDict()
+data.header.magic = font.header.magic
+data.header.version = font.header.version
+data.header.type = font.header.type
+data.header.filesize = font.header.filesize
+data.header.filetime = font.header.filetime
+data.header.filedate = font.header.filedate
+data.header.foo_1 = font.header.foo_1
+data.header.foo_2 = font.header.foo_2
+data.header.crc = font.header.crc
 
-for index, offset in enumerate(font.fat.offsets):
-	print "Offset #%d: %d" % (index, offset)
+print data.dumps()
 
-for index, font in enumerate(font.fonts):
-	if font:
-		print "Font #%d: offset=%d, datalength=%d, height=%d" % (index, font.offset, font.datalength, font.height)
+#print "Count: %d" % font.fat.count
 
-		#print "\tColors"
-		#for index, color in enumerate(font.colors):
-			#print "\t\tColor #%d: R=%d, G=%d, B=%d" % (index, color.r, color.g, color.b)
+#for index, offset in enumerate(font.fat.offsets):
+	#print "Offset #%d: %d" % (index, offset)
 
-		print "\tCharacters"
-		for index, character in enumerate(font.characters):
-			if character.width:
-				print "\t\tCharacter #%d: offset=%d, width=%d" % (index, character.offset, character.width)
+#for index, font in enumerate(font.fonts):
+	#if font:
+		#print "Font #%d: offset=%d, datalength=%d, height=%d" % (index, font.offset, font.datalength, font.height)
 
-		print "\tMatrices"
-		for index, matrix in enumerate(font.matrices):
-			if matrix:
-				print "\t\tMatrix #%d: width=%d, height=%d" % (index, matrix.width, matrix.height)
+		##print "\tColors"
+		##for index, color in enumerate(font.colors):
+			##print "\t\tColor #%d: R=%d, G=%d, B=%d" % (index, color.r, color.g, color.b)
 
-				print "\t\t\tRows"
-				for index, row in enumerate(matrix.rows):
-					print "\t\t\t\tRow #%d:" % (index)
+		#print "\tCharacters"
+		#for index, character in enumerate(font.characters):
+			#if character.width:
+				#print "\t\tCharacter #%d: offset=%d, width=%d" % (index, character.offset, character.width)
 
-					print "\t\t\t\t\tColumns"
-					for index, column in enumerate(row.columns):
-						print "\t\t\t\t\t\tColumn #%d: %d" % (index, column)
+		#print "\tMatrices"
+		#for index, matrix in enumerate(font.matrices):
+			#if matrix:
+				#print "\t\tMatrix #%d: width=%d, height=%d" % (index, matrix.width, matrix.height)
+
+				#print "\t\t\tRows"
+				#for index, row in enumerate(matrix.rows):
+					#print "\t\t\t\tRow #%d:" % (index)
+
+					#print "\t\t\t\t\tColumns"
+					#for index, column in enumerate(row.columns):
+						#print "\t\t\t\t\t\tColumn #%d: %d" % (index, column)
