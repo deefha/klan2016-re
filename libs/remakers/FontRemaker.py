d179404fa52ca182fec355dd7b9b876e5cb27841
Daniel Felix Hrouzek <dhrouzek@rozhlas.cz>
2017-10-20 10:25:50 +0200

    Generování přehledové matice fontu

--- libs/remakers/FontRemaker.py
+++ libs/remakers/FontRemaker.py
@@ -14,16 +14,6 @@ class FontRemaker(CommonRemaker):
 	PATTERN_FILE_MATRIX = "%s%03d.bin"
 
 	def export_objects(self):
-		#print self.meta.header
-
-		#for item, value in self.meta.header.iteritems():
-			#print item, value
-
-		#print self.meta.fat.offsets
-
-		#for offset_index, offset in self.meta.fat.offsets.iteritems():
-			#print offset
-
 		for font_index, font in self.meta.data.fonts.iteritems():
 			if font.content:
 				path_characters = "%s%02d/characters/" % (self.PATH_OBJECTS, int(font_index))
@@ -34,11 +24,19 @@ class FontRemaker(CommonRemaker):
 				with open(font.content.colormap.replace("blobs://", self.ROOT_BLOBS), "rb") as f:
 					font_colormap = f.read()
 
+				i_font = Image.new("RGBA", (16 * font.content.height, 16 * font.content.height), (255, 255, 255, 0))
+
 				for matrix_index, matrix in font.content.matrices.iteritems():
 					if matrix.content:
 						with open(matrix.content.replace("blobs://", self.ROOT_BLOBS), "rb") as f:
 							matrix_content = f.read()
 
-						i = Image.frombytes("P", (font.content.characters[matrix_index].computed_width, font.content.height), matrix_content)
-						i.putpalette(font_colormap)
-						i.save("%s%03d.gif" % (path_characters, int(matrix_index)))
+						i_character = Image.frombytes("P", (font.content.characters[matrix_index].computed_width, font.content.height), matrix_content)
+						i_character.putpalette(font_colormap)
+						i_character.save("%s%03d.gif" % (path_characters, int(matrix_index)))
+						i_character.convert("RGBA")
+
+						i_font.paste(i_character, ((int(matrix_index) % 16) * font.content.height, (int(matrix_index) // 16) * font.content.height))
+
+				i_font.save("%s%02d/font.gif" % (self.PATH_OBJECTS, int(font_index)), transparency = 0)
+				i_font.save("%s%02d/font.png" % (self.PATH_OBJECTS, int(font_index)))
