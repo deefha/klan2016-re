ce3a44ab9a83a4202a974302bd80ab5cd0e26c2c
Daniel Felix Hrouzek <dhrouzek@rozhlas.cz>
2018-01-16 16:40:18 +0100

    source_remake: hromadné operace

--- scripts/source_remake.py
+++ scripts/source_remake.py
@@ -5,7 +5,8 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../libs/")
 
 from remakers import *
 
-
+ISSUES = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32-33", "34", "35", "36", "37", "38", "39", "40", "41", "42"]
+SOURCES = ["font", "font2", "font_lt", "font2_lt", "imgs", "image1", "cache"]
 
 if len(sys.argv) != 3:
 	# TODO message
@@ -16,20 +17,58 @@ ARG_SOURCE = sys.argv[2]
 
 
 
-print "Issue: %s" % ARG_ISSUE
-print "Source: %s" % ARG_SOURCE
+def remake_issue(issue, source):
+	if (source == "all"):
+		for source in SOURCES:
+			remake_source(issue, source)
+	else:
+		remake_source(issue, source)
 
-if ARG_SOURCE == "font" or ARG_SOURCE == "font2" or ARG_SOURCE == "font_lt" or ARG_SOURCE == "font2_lt":
-	remaker = FontRemaker.FontRemaker(ARG_ISSUE, ARG_SOURCE)
-
-elif ARG_SOURCE == "imgs" or ARG_SOURCE == "image1" or ARG_SOURCE == "cache":
-	remaker = ImgsRemaker.ImgsRemaker(ARG_ISSUE, ARG_SOURCE)
-
-else:
-	sys.exit()
+	return True
 
 
 
-remaker.fill_scheme()
-remaker.export_scheme()
-remaker.export_assets()
+def remake_source(issue, source):
+	if issue < "01" and source == "font2":
+		return False
+	if issue < "01" and source == "image1":
+		return False
+	if issue < "28" and source == "font_lt":
+		return False
+	if issue < "28" and source == "font2_lt":
+		return False
+	if issue < "28" and source == "cache":
+		return False
+	if issue >= "28" and source == "image1":
+		return False
+
+	print "Issue: %s" % issue
+	print "Source: %s" % source
+
+	if source == "font" or source == "font2" or source == "font_lt" or source == "font2_lt":
+		remaker = FontRemaker.FontRemaker(issue, source)
+
+	elif source == "imgs" or source == "image1" or source == "cache":
+		remaker = ImgsRemaker.ImgsRemaker(issue, source)
+
+	else:
+		return False
+
+	remaker.fill_scheme()
+	remaker.export_scheme()
+	remaker.export_assets()
+
+	return True
+
+
+
+def main():
+	if ARG_ISSUE == "all":
+		for issue in ISSUES:
+			remake_issue(issue, ARG_SOURCE)
+	else:
+		remake_issue(ARG_ISSUE, ARG_SOURCE)
+
+
+
+main()
