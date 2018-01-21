9ae9ff38d38a0114c2dc283d102a50f54158ffa8
Daniel Felix Hrouzek <de.ef.ha@gmail.com>
2018-01-21 18:16:25 +0100

    mods decompiler

--- libs/decompilers/ModsDecompiler.py
+++ libs/decompilers/ModsDecompiler.py
@@ -7,8 +7,12 @@ from CommonDecompiler import CommonDecompiler
 
 class ModsDecompiler(CommonDecompiler):
 
-	PATTERN_PATH_WAVE = "%s%04d/"
-	PATTERN_FILE_CONTENT = "%s%04d/content.bin"
+	PATTERN_PATH_MOD = "%s/mods/%04d/"
+	PATTERN_PATH_MOD_PATTERNS = "%s/mods/%04d/patterns/"
+	PATTERN_PATH_SAMPLE = "%s/samples/"
+
+	PATTERN_FILE_MOD_PATTERN = "%s/mods/%04d/patterns/%04d.bin"
+	PATTERN_FILE_SAMPLE = "%s/samples/%04d.bin"
 
 	def fill_meta_data(self):
 		super(ModsDecompiler, self).fill_meta_data()
@@ -21,48 +25,86 @@ class ModsDecompiler(CommonDecompiler):
 
 		self.meta.data.mods = ObjDict()
 
-		#for mods_index, mod in enumerate(self.library.data.mods):
-			#data_mod = ObjDict()
-			#data_mod.param_offset = mod.param_offset
-			#data_mod.content = ObjDict()
+		for mod_index, mod in enumerate(self.library.data.mods):
+			data_mod = ObjDict()
+			data_mod.param_offset = mod.param_offset
+			data_mod.content = ObjDict()
 
-			#if mod.content:
-				#print "Wave #%d: param_offset=%d, data_size=%d, mod_size=%d, mode=%d" % (mods_index, mod.param_offset, mod.content.data_size, mod.content.mod_size, mod.content.mode)
+			if mod.content:
+				print "Mod #%d: param_offset=%d, name='%s', count_positions=%d, count_patterns=%d, count_samples=%d" % (mod_index, mod.param_offset, mod.content.name, mod.content.count_positions, mod.content.count_patterns, mod.content.count_samples)
 
-				#file_content = self.PATTERN_FILE_CONTENT % (self.PATH_BLOBS, mods_index)
+				path_mod = self.PATTERN_PATH_MOD % (self.PATH_BLOBS, mod_index)
+				path_mod_patterns = self.PATTERN_PATH_MOD_PATTERNS % (self.PATH_BLOBS, mod_index)
 
-				#path_mod = self.PATTERN_PATH_WAVE % (self.PATH_BLOBS, mods_index)
+				if not os.path.exists(path_mod):
+					os.makedirs(path_mod)
 
-				#if not os.path.exists(path_mod):
-					#os.makedirs(path_mod)
+				if not os.path.exists(path_mod_patterns):
+					os.makedirs(path_mod_patterns)
 
-				#data_mod.content.data_size = mod.content.data_size
-				#data_mod.content.mod_size = mod.content.mod_size
-				#data_mod.content.mode = mod.content.mode
-				#data_mod.content.foo_1 = mod.content.foo_1
-				#data_mod.content.foo_2 = mod.content.foo_2
-				#data_mod.content.data = ObjDict()
-				#data_mod.content.data.param_data_size = mod.content.data.param_data_size
+				#data_mod.content.name = mod.content.name
+				data_mod.content.name = ""
+				data_mod.content.count_positions = mod.content.count_positions
+				data_mod.content.count_patterns = mod.content.count_patterns
+				data_mod.content.count_samples = mod.content.count_samples
+				#data_mod.content.foo = mod.content.foo
+				data_mod.content.foo = ""
 
-				#if self.issue > "00":
-					#data_mod.content.data.title = ""
-					##data_mod.content.data.title = mod.content.data.title
-				#else:
-					#data_mod.content.data.title = ""
+				data_mod.content.data = ObjDict()
+				data_mod.content.data.param_count_patterns = mod.content.data.param_count_patterns
+				data_mod.content.data.positions = mod.content.data.positions
+				data_mod.content.data.patterns = ObjDict()
 
-				#data_mod.content.data.content = "blobs://%s/%s/%04d/content.bin" % (self.issue, self.source, mods_index)
+				for pattern_index, pattern in enumerate(mod.content.data.patterns):
+					file_pattern = self.PATTERN_FILE_MOD_PATTERN % (self.PATH_BLOBS, mod_index, pattern_index)
 
-				#print "\tContent"
-				#f = open(file_content, "wb")
-				#f.write(mod.content.data.content)
-				#f.close
+					data_mod.content.data.patterns[str(pattern_index)] = "blobs://%s/%s/mods/%04d/patterns/%04d.bin" % (self.issue, self.source, mod_index, pattern_index)
 
-			#else:
-				#print "Wave #%d: param_offset=%d, no content" % (mods_index, mod.param_offset)
+					print "\tPattern #%d" % pattern_index
+					f = open(file_pattern, "wb")
+					f.write(pattern)
+					f.close
 
-			#self.meta.data.mods[str(mods_index)] = data_mod
+			else:
+				print "Mod #%d: param_offset=%d, no content" % (mod_index, mod.param_offset)
+
+			self.meta.data.mods[str(mod_index)] = data_mod
 
 		self.meta.data.samples = ObjDict()
+
+		for sample_index, sample in enumerate(self.library.data.samples):
+			data_sample = ObjDict()
+			data_sample.param_offset = sample.param_offset
+			data_sample.content = ObjDict()
+
+			if sample.content:
+				print "Sample #%d: param_offset=%d" % (sample_index, sample.param_offset)
+
+				path_sample = self.PATTERN_PATH_SAMPLE % (self.PATH_BLOBS)
+
+				if not os.path.exists(path_sample):
+					os.makedirs(path_sample)
+
+				data_sample.content.data_size = sample.content.data_size
+				#data_sample.content.foo = sample.content.foo
+				data_sample.content.foo = ""
+
+				data_sample.content.data = ObjDict()
+				data_sample.content.data.param_data_size = sample.content.data.param_data_size
+
+				file_sample = self.PATTERN_FILE_SAMPLE % (self.PATH_BLOBS, sample_index)
+
+				data_sample.content.data.content = "blobs://%s/%s/samples/%04d.bin" % (self.issue, self.source, sample_index)
+
+				print "\tContent"
+				f = open(file_sample, "wb")
+				f.write(sample.content.data.content)
+				f.close
+
+			else:
+				print "Sample #%d: param_offset=%d, no content" % (sample_index, sample.param_offset)
+
+			self.meta.data.samples[str(sample_index)] = data_sample
 
 
 
