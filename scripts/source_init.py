402b215a0c3fd3030196abaac2cd85105e62e722
Daniel Felix Hrouzek <de.ef.ha@gmail.com>
2018-01-29 23:03:24 +0100

    source_init (ISO)

--- scripts/source_init.py
+++ scripts/source_init.py
@@ -6,8 +6,14 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../libs/")
 from pprint import pprint
 import pycdlib
 import requests
+import hashlib
+import datetime
 from io import BytesIO
 from tqdm import tqdm
+from yaml import load
+from objdict import ObjDict
+from colorama import init, Fore, Back, Style
+init(autoreset=True)
 
 from structs.klan_mods_v1 import KlanModsV1
 
@@ -26,19 +32,113 @@ from structs.klan_mods_v1 import KlanModsV1
 
 #library = KlanModsV1.from_io(extracted)
 
-session = requests.Session()
-response = session.get("https://docs.google.com/uc?export=download", params = { "id": "0Bw1iZK6iA4_xT1hUTjJ3Zkwwa0k" }, stream = True)
+# load YML data config
+with open("../data/config.yml") as f:
+	config_yaml = load(f)
 
-for key, value in response.cookies.items():
-	if key.startswith("download_warning"):
-		response = session.get("https://docs.google.com/uc?export=download", params = { "id": "0Bw1iZK6iA4_xT1hUTjJ3Zkwwa0k", "confirm": value }, stream = True)
+# convert dict to ObjDict
+config = ObjDict()
+config.origin = ObjDict()
+config.origin.main = config_yaml["origin"]["main"]
+config.origin.confirm = config_yaml["origin"]["confirm"]
+config.origin.confirm_key = config_yaml["origin"]["confirm_key"]
+config.issues = ObjDict()
 
-response_size = 575526912
-chunk_size = 32 * 1024
+for issue_yaml in config_yaml["issues"]:
+	issue = ObjDict()
+	issue.id = issue_yaml["id"]
+	issue.origin = ObjDict()
+	issue.origin.id = issue_yaml["origin"]["id"]
+	issue.origin.size = issue_yaml["origin"]["size"]
+	issue.origin.md5 = issue_yaml["origin"]["md5"]
 
-with open("./download.iso", "wb") as f:
-	with tqdm(total=response_size, unit='B', unit_scale=True, unit_divisor=1024, ascii=True) as pbar: 
-		for chunk in response.iter_content(chunk_size):
-			if chunk:
-				f.write(chunk)
-				pbar.update(len(chunk))
+	config.issues[str(issue.id)] = issue
+
+# iterate over issues in config
+for issue_id, issue in config.issues.iteritems():
+	print Fore.BLACK + Back.GREEN + "Issue #%s" % issue.id
+
+	issue_path = "../data/sources/%s.iso" % issue.id
+	check_path = "../data/sources/%s.check" % issue.id
+
+	# download missing issues
+	if os.path.isfile(issue_path):
+		print "\tSource exists"
+	else:
+		print "\tDownloading source..."
+
+		session = requests.Session()
+		response = session.get(config.origin.main % issue.origin.id, stream = True)
+
+		for key, value in response.cookies.items():
+			if key.startswith(config.origin.confirm_key):
+				response = session.get(config.origin.confirm % (issue.origin.id, value), stream = True)
+
+		with open(issue_path, "wb") as f:
+			with tqdm(total=issue.origin.size, unit="B", unit_scale=True, unit_divisor=1024, ascii=True) as pbar: 
+				for chunk in response.iter_content(32 * 1024):
+					if chunk:
+						f.write(chunk)
+						pbar.update(len(chunk))
+
+	# skip checking if checked
+	if os.path.isfile(check_path):
+		with open(check_path, "r") as f:
+			check_date = f.read()
+
+		print Fore.GREEN + "\tChecked already (%s)" % check_date
+		continue
+
+	# check size by config
+	if not os.path.isfile(issue_path):
+		print Fore.RED + "\tCan not check size"
+		continue
+	else:
+		print "\tChecking size..."
+		issue_size = os.path.getsize(issue_path)
+
+		if issue_size == issue.origin.size:
+			print "\tSize OK (%s)" % issue_size
+		else:
+			print "\tSize error (%s != %s)" % (issue_size, issue.origin.size)
+			continue
+
+	# check md5 by config
+	if not os.path.isfile(issue_path):
+		print Fore.RED + "\tCan not check MD5 signature"
+		continue
+	else:
+		print "\tChecking MD5 signature..."
+
+		hash_md5 = hashlib.md5()
+
+		with tqdm(total=issue_size, unit="B", unit_scale=True, ascii=True, leave=False) as t:
+			chunk_size = 4096
+
+			with open(issue_path, "rb") as f:
+				for chunk in iter(lambda: f.read(chunk_size), b""):
+					hash_md5.update(chunk)
+					t.update(len(chunk))
+
+			t.update(abs(issue_size - t.n))
+			t.close()
+
+		issue_md5 = hash_md5.hexdigest()
+
+		if issue_md5 == issue.origin.md5:
+			print "\tMD5 signature OK (%s)" % issue_md5
+		else:
+			print Fore.RED + "\tMD5 signature error (%s != %s)" % (issue_md5, issue.origin.md5)
+			continue
+
+	# write check file
+	with open(check_path, "w") as f:
+		print "\tWriting check file..."
+		f.write(datetime.datetime.utcnow().replace(microsecond=0).isoformat())
+
+	# issue done
+	if not os.path.isfile(issue_path):
+		print Fore.RED + "\tChecking error"
+		continue
+	else:
+		print Fore.GREEN + "\tChecking OK"
