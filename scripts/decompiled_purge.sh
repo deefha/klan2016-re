74ee0f89899eb0f55b068af8f814e810a5cc3de0
Daniel Felix Hrouzek <dhrouzek@rozhlas.cz>
2018-01-31 12:36:42 +0100

    Přepsání čistícího skriptu z Bashe do Pythonu, sjednocení

--- scripts/decompiled_purge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find ../data/blobs -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
-find ../data/meta -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
--- scripts/remaked_purge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find ../data/assets -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
-find ../data/schemes -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
