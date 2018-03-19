cc84ccd965c7b7645656d9de2d426d798c9e0c85
Daniel Felix Hrouzek <dhrouzek@rozhlas.cz>
2018-03-19 17:35:05 +0100

    fonty se ukládají už jen jako RGBA PNG s vymaskovanou transparentností

--- libs/remakers/FontsRemaker.py
+++ libs/remakers/FontsRemaker.py
@@ -1,6 +1,7 @@
 # common imports
 import os, sys, datetime
 from objdict import ObjDict
+from pprint import pprint
 
 # specific imports
 from PIL import Image
@@ -24,6 +25,8 @@ class FontsRemaker(CommonRemaker):
 				with open(font.content.colormap.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
 					font_colormap = f.read()
 
+				transparent_r, transparent_g, transparent_b = bytearray(font_colormap)[765:768]
+
 				i_font = Image.new("RGBA", (16 * font.content.height, 16 * font.content.height), (255, 255, 255, 0))
 
 				for matrix_index, matrix in font.content.matrices.iteritems():
@@ -33,12 +36,19 @@ class FontsRemaker(CommonRemaker):
 
 						i_character = Image.frombytes("P", (font.content.characters[matrix_index].computed_width, font.content.height), matrix_content)
 						i_character.putpalette(font_colormap)
-						i_character.save("%s%03d.gif" % (path_characters, int(matrix_index)))
-						i_character.convert("RGBA")
 
-						i_font.paste(i_character, ((int(matrix_index) % 16) * font.content.height, (int(matrix_index) // 16) * font.content.height))
+						i_character_new = i_character.convert("RGBA")
+						i_character_new_pixels = i_character_new.load()
+						i_character_new_width, i_character_new_height = i_character_new.size
 
-				i_font.save("%s%02d/%s.gif" % (self.PATH_DATA_REMAKED, int(font_index), self.source.library), transparency = 0) # TODO path
+						for x in range(i_character_new_width):
+							for y in range(i_character_new_height):
+								if i_character_new_pixels[x, y] == (transparent_r, transparent_g, transparent_b, 255):
+									i_character_new_pixels[x, y] = (255, 255, 255, 0)
+
+						i_character_new.save("%s%03d.png" % (path_characters, int(matrix_index)))
+						i_font.paste(i_character_new, ((int(matrix_index) % 16) * font.content.height, (int(matrix_index) // 16) * font.content.height))
+
 				i_font.save("%s%02d/%s.png" % (self.PATH_DATA_REMAKED, int(font_index), self.source.library)) # TODO path
 
 				if status:
@@ -59,14 +69,14 @@ class FontsRemaker(CommonRemaker):
 
 				data_font = ObjDict()
 				data_font.height = font.content.height
-				data_font.asset = "remaked://%s/%s/%s/%02d/font.gif" % (self.issue.number, self.source.library, self.source_index, int(font_index))
+				data_font.asset = "remaked://%s/%s/%s/%02d/font.png" % (self.issue.number, self.source.library, self.source_index, int(font_index))
 				data_font.characters = ObjDict()
 
 				for matrix_index, matrix in font.content.matrices.iteritems():
 					if matrix.content:
 						data_matrix = ObjDict()
 						data_matrix.width = font.content.characters[matrix_index].computed_width
-						data_matrix.asset = "%s%03d.gif" % (path_characters, int(matrix_index))
+						data_matrix.asset = "%s%03d.png" % (path_characters, int(matrix_index))
 
 						data_font.characters[matrix_index] = data_matrix
 
