e604815b1207bd0a6a0ae3a1fd7d19e88583929b
Daniel Felix Hrouzek <dhrouzek@rozhlas.cz>
2018-03-20 17:25:22 +0100

    texts_v1 remaker: generování "plachet"

--- libs/remakers/TextsRemaker.py
+++ libs/remakers/TextsRemaker.py
@@ -22,6 +22,13 @@ class TextsRemaker(CommonRemaker):
 
 
 	def export_assets(self):
+		with open("%s%s/%s/%s.json" % (self.PATH_PHASE_REMAKED, self.issue.number, "fonts", 0), "r") as f:
+			#content = f.read()
+			lines = f.readlines() # TODO
+			content = ''.join(lines) # TODO
+
+			self.fonts_0 = ObjDict(content)
+
 		for text_index, text in tqdm(self.meta_decompiled.data.texts.iteritems(), total=len(self.meta_decompiled.data.texts), desc="data.texts", ascii=True, leave=False, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]"):
 			self.items_total += 1
 			status = True
@@ -29,7 +36,15 @@ class TextsRemaker(CommonRemaker):
 			data_text = u""
 
 			if text.linetable:
+				lines_width = 0
+				lines_height = 0
+				lines = []
+
 				for linetable_index, linetable in text.linetable.iteritems():
+					line_width = 0
+					line_height = text.linetable_meta[linetable_index].content.height
+					line_pieces = []
+
 					if linetable.content.pieces:
 						for piece_index, piece in linetable.content.pieces.iteritems():
 							# konec radku
@@ -53,6 +68,10 @@ class TextsRemaker(CommonRemaker):
 							# mezera
 							elif piece.raw == 32:
 								data_text += " "
+
+								i_piece = Image.new("RGBA", (piece.data.length, line_height), (0, 0, 0, 0))
+								line_width += piece.data.length
+								line_pieces.append(i_piece)
 							# bezny znak
 							else:
 								if piece.raw < 128:
@@ -60,85 +79,40 @@ class TextsRemaker(CommonRemaker):
 								else:
 									data_text += self.chartable[piece.raw - 128]
 
+								#print "%s: %s, %s" % (text_index, linetable_index, piece_index)
+								if str(piece.raw) in self.fonts_0.fonts["1"].characters:
+									i_piece = Image.open("%s%s/%s/%s/%02d/normal/%03d.png" % (self.PATH_PHASE_REMAKED, self.issue.number, "fonts", 0, 1, piece.raw))
+									i_piece_width, i_piece_height = i_piece.size
+									line_width += i_piece_width
+									line_pieces.append(i_piece)
+
+					i_line = Image.new("RGBA", (line_width, line_height), (0, 0, 0, 0))
+					line_offset_x = 0
+
+					for line_piece in line_pieces:
+						line_piece_width, line_piece_height = line_piece.size
+						i_line.paste(line_piece, (line_offset_x, 0))
+						line_offset_x += line_piece_width
+
+					if line_width > lines_width:
+						lines_width = line_width
+					lines_height += line_height
+					lines.append(i_line)
+
+				i_lines_temp = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 0))
+				lines_offset_y = 0
+
+				for line in lines:
+					line_width, line_height = line.size
+					i_lines_temp.paste(line, (0, lines_offset_y))
+					lines_offset_y += line_height
+
+				i_lines = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 255))
+				i_lines = Image.alpha_composite(i_lines, i_lines_temp)
+				i_lines.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
+
 			with open("%s%04d.txt" % (self.PATH_DATA_REMAKED, int(text_index)), "w") as f:
 				f.write(data_text.encode("utf8", "replace"))
-
-					#i = Image.frombytes("RGB", (text.content.width, text.content.height), text_content, "raw", "BGR;16")
-					#i.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
-
-				## colormap, indexed, RLE compression
-				## 1 (#00+)
-				## 257 (#09+)
-				#if text.content.mode == 1 or text.content.mode == 257:
-					#with open(text.content.data.colormap.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-						#text_colormap = f.read()
-
-					#with open(text.content.data.content.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-						#text_content = f.read()
-
-					#text_content_unpacked = []
-					#content_byte_break = True
-					#content_byte_break_length = None
-					#content_byte_break_count = None
-
-					#for content_byte in text_content:
-						#if content_byte_break:
-							#if ord(content_byte) > 127:
-								#content_byte_break_length = ord(content_byte) - 127
-								#content_byte_break_count = None
-							#else:
-								#content_byte_break_length = None
-								#content_byte_break_count = ord(content_byte) + 1
-
-							#content_byte_break = False
-						#else:
-							#if content_byte_break_count:
-								#text_content_unpacked.extend([content_byte] * content_byte_break_count)
-								#content_byte_break = True
-							#else:
-								#text_content_unpacked.append(content_byte)
-								#content_byte_break_length -= 1
-								
-								#if not content_byte_break_length:
-									#content_byte_break = True
-
-					#text_content_unpacked = "".join(text_content_unpacked)
-
-					#i = Image.frombytes("P", (text.content.width, text.content.height), text_content_unpacked)
-					#i.putpalette(text_colormap)
-					#i.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
-
-				## colormap, indexed, no compression
-				## 256 (#00+)
-				#elif text.content.mode == 256:
-					#with open(text.content.data.colormap.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-						#text_colormap = f.read()
-
-					#with open(text.content.data.content.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-						#text_content = f.read()
-
-						#i = Image.frombytes("P", (text.content.width, text.content.height), text_content)
-						#i.putpalette(text_colormap)
-						#i.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
-
-				## TODO, red placeholder
-				## 4 (#06+)
-				## 258 (#11+)
-				#elif text.content.mode == 4 or text.content.mode == 258:
-					#status = False
-
-					#i = Image.new("RGB", (text.content.width, text.content.height), (255, 0, 0))
-					#i.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
-
-				## RGB565
-				## 5 (#11+)
-				## 261 (#11+)
-				#elif text.content.mode == 5 or text.content.mode == 261:
-					#with open(text.content.data.content.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-						#text_content = f.read()
-
-						#i = Image.frombytes("RGB", (text.content.width, text.content.height), text_content, "raw", "BGR;16")
-						#i.save("%s%04d.png" % (self.PATH_DATA_REMAKED, int(text_index)))
 
 			if status:
 				self.items_hit += 1
