a35048dd12a356013428467186c415e044b1f5f1
Daniel Felix Hrouzek <de.ef.ha@gmail.com>
2018-03-23 23:43:07 +0100

    texts remaker: plaintext varianta

--- libs/remakers/TextsRemaker.py
+++ libs/remakers/TextsRemaker.py
@@ -69,135 +69,146 @@ class TextsRemaker(CommonRemaker):
 					if not os.path.exists(path_text):
 						os.makedirs(path_text)
 
-					data_text = u""
+					if int(variant_index) == 0 or int(variant_index) == 1:
+						data_text = u""
 
-					if variant.content.linetable:
-						lines_width = 0
-						lines_height = 0
-						lines = []
+						if variant.content.linetable:
+							lines_width = 0
+							lines_height = 0
+							lines = []
 
-						for linetable_index, linetable in variant.content.linetable.iteritems():
-							line_width = 0
-							line_height = variant.content.linetable_meta[linetable_index].content.height
-							line_pieces = []
+							for linetable_index, linetable in variant.content.linetable.iteritems():
+								line_width = 0
+								line_height = variant.content.linetable_meta[linetable_index].content.height
+								line_pieces = []
 
-							flag_bold = False
-							flag_italic = False
-							flag_link = False
+								flag_bold = False
+								flag_italic = False
+								flag_link = False
 
-							font_id = 1
+								font_id = 1
 
-							if linetable.content.pieces:
-								for piece_index, piece in linetable.content.pieces.iteritems():
-									# konec radku
-									if piece.raw == 0:
-										data_text += "\n"
-									# font
-									elif piece.raw == 1:
-										font_id = piece.data.mode
-										continue
-									# bold
-									elif piece.raw == 2:
-										flag_bold = not flag_bold
-										continue
-									# italic
-									elif piece.raw == 4:
-										flag_italic = not flag_italic
-										continue
-									# obrazek
-									elif piece.raw == 8:
-										image_content_unpacked = []
+								if linetable.content.pieces:
+									for piece_index, piece in linetable.content.pieces.iteritems():
+										# konec radku
+										if piece.raw == 0:
+											data_text += "\n"
+										# font
+										elif piece.raw == 1:
+											font_id = piece.data.mode
+										# bold
+										elif piece.raw == 2:
+											flag_bold = not flag_bold
+										# italic
+										elif piece.raw == 4:
+											flag_italic = not flag_italic
+										# obrazek
+										elif piece.raw == 8:
+											image_content_unpacked = []
 
-										for row_index, row in piece.data.rows.iteritems():
-											content_byte_count = None
+											for row_index, row in piece.data.rows.iteritems():
+												content_byte_count = None
 
-											with open(row.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-												row_content = f.read()
+												with open(row.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
+													row_content = f.read()
 
-											for content_byte in row_content[:-1]:
-												if content_byte_count:
-													image_content_unpacked.extend([content_byte] * content_byte_count)
-													content_byte_count = None
-												else:
-													if ord(content_byte) > 192:
-														content_byte_count = ord(content_byte) - 192
+												for content_byte in row_content[:-1]:
+													if content_byte_count:
+														image_content_unpacked.extend([content_byte] * content_byte_count)
+														content_byte_count = None
 													else:
-														image_content_unpacked.append(content_byte)
+														if ord(content_byte) > 192:
+															content_byte_count = ord(content_byte) - 192
+														else:
+															image_content_unpacked.append(content_byte)
 
-										image_content_unpacked = "".join(image_content_unpacked)
+											image_content_unpacked = "".join(image_content_unpacked)
 
-										with open(variant.content.palettetable[str(piece.data.table)].content.data.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-											piece_colormap = f.read()
+											with open(variant.content.palettetable[str(piece.data.table)].content.data.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
+												piece_colormap = f.read()
 
-										i_piece = Image.frombytes("P", (piece.data.width, piece.data.height), image_content_unpacked)
-										i_piece.putpalette(piece_colormap)
-										i_piece.convert("RGBA")
-										line_width += piece.data.width
-										line_pieces.append(i_piece)
-										continue
-									# odkaz
-									elif piece.raw == 9:
-										flag_link = not flag_link
-										continue
-									# mezera
-									elif piece.raw == 32:
-										data_text += " "
-
-										i_piece = Image.new("RGBA", (piece.data.length, line_height), (0, 0, 0, 0))
-										line_width += piece.data.length
-										line_pieces.append(i_piece)
-									# bezny znak
-									else:
-										if piece.raw < 128:
-											data_text += chr(piece.raw)
-										else:
-											data_text += self.CHARTABLE[piece.raw - 128]
-
-										if str(piece.raw) in self.fonts[str(variant_index)].fonts[str(font_id)].normal.characters:
-											if flag_bold:
-												font_variant = "bold"
-											elif flag_italic:
-												font_variant = "italic"
-											else:
-												font_variant = "normal"
-
-											if flag_link:
-												font_variant += "_link"
-
-											i_piece = self.fonts[str(variant_index)].fonts[str(font_id)][font_variant].characters[str(piece.raw)].image
-											#i_piece = Image.open(self.fonts[str(variant_index)].fonts[str(font_id)][font_variant].characters[str(piece.raw)].asset.replace("remaked://", self.PATH_PHASE_REMAKED))
-											i_piece_width, i_piece_height = i_piece.size
-											line_width += i_piece_width
+											i_piece = Image.frombytes("P", (piece.data.width, piece.data.height), image_content_unpacked)
+											i_piece.putpalette(piece_colormap)
+											i_piece.convert("RGBA")
+											line_width += piece.data.width
 											line_pieces.append(i_piece)
+										# odkaz
+										elif piece.raw == 9:
+											flag_link = not flag_link
+										# mezera
+										elif piece.raw == 32:
+											data_text += " "
 
-							i_line = Image.new("RGBA", (line_width, line_height), (0, 0, 0, 0))
-							line_offset_x = 0
+											i_piece = Image.new("RGBA", (piece.data.length, line_height), (0, 0, 0, 0))
+											line_width += piece.data.length
+											line_pieces.append(i_piece)
+										# bezny znak
+										else:
+											if piece.raw < 128:
+												data_text += chr(piece.raw)
+											else:
+												data_text += self.CHARTABLE[piece.raw - 128]
 
-							for line_piece in line_pieces:
-								line_piece_width, line_piece_height = line_piece.size
-								i_line.paste(line_piece, (line_offset_x, 0))
-								line_offset_x += line_piece_width
+											if str(piece.raw) in self.fonts[str(variant_index)].fonts[str(font_id)].normal.characters:
+												if flag_bold:
+													font_variant = "bold"
+												elif flag_italic:
+													font_variant = "italic"
+												else:
+													font_variant = "normal"
 
-							if line_width > lines_width:
-								lines_width = line_width
-							lines_height += line_height
-							lines.append(i_line)
+												if flag_link:
+													font_variant += "_link"
 
-						i_lines_temp = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 0))
-						lines_offset_y = 0
+												i_piece = self.fonts[str(variant_index)].fonts[str(font_id)][font_variant].characters[str(piece.raw)].image
+												#i_piece = Image.open(self.fonts[str(variant_index)].fonts[str(font_id)][font_variant].characters[str(piece.raw)].asset.replace("remaked://", self.PATH_PHASE_REMAKED))
+												i_piece_width, i_piece_height = i_piece.size
+												line_width += i_piece_width
+												line_pieces.append(i_piece)
 
-						for line in lines:
-							line_width, line_height = line.size
-							i_lines_temp.paste(line, (0, lines_offset_y))
-							lines_offset_y += line_height
+								i_line = Image.new("RGBA", (line_width, line_height), (0, 0, 0, 0))
+								line_offset_x = 0
 
-						i_lines = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 255))
-						i_lines = Image.alpha_composite(i_lines, i_lines_temp)
-						i_lines.convert("RGBA")
-						i_lines.save(self.PATTERN_FILE_TEXT_ASSET % (path_text, int(variant_index)))
+								for line_piece in line_pieces:
+									line_piece_width, line_piece_height = line_piece.size
+									i_line.paste(line_piece, (line_offset_x, 0))
+									line_offset_x += line_piece_width
 
-					with open(self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index)), "w") as f:
-						f.write(data_text.encode("utf8", "replace"))
+								if line_width > lines_width:
+									lines_width = line_width
+								lines_height += line_height
+								lines.append(i_line)
+
+							i_lines_temp = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 0))
+							lines_offset_y = 0
+
+							for line in lines:
+								line_width, line_height = line.size
+								i_lines_temp.paste(line, (0, lines_offset_y))
+								lines_offset_y += line_height
+
+							i_lines = Image.new("RGBA", (lines_width, lines_height), (0, 0, 0, 255))
+							i_lines = Image.alpha_composite(i_lines, i_lines_temp)
+							i_lines.convert("RGBA")
+							i_lines.save(self.PATTERN_FILE_TEXT_ASSET % (path_text, int(variant_index)))
+
+						with open(self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index)), "w") as f:
+							f.write(data_text.encode("utf8", "replace"))
+
+					else:
+						with open(variant.content.data.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
+							data_text_raw = f.read()
+
+						data_text = ""
+
+						for char_index, char in enumerate(data_text_raw):
+							if ord(char) < 128:
+								data_text += char
+							else:
+								data_text += self.CHARTABLE[ord(char) - 128]
+
+						with open(self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index)), "w") as f:
+							f.write(data_text.encode("utf8", "replace"))
 
 				if status:
 					self.items_hit += 1
@@ -239,93 +250,102 @@ class TextsRemaker(CommonRemaker):
 
 					data_variant = ObjDict()
 
-					data_variant.asset = (self.PATTERN_FILE_TEXT_ASSET % (path_text, int(variant_index))).replace(self.PATH_PHASE_REMAKED, "remaked://")
-					data_variant.plain = (self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index))).replace(self.PATH_PHASE_REMAKED, "remaked://")
+					if variant_index == 0 or  variant_index == 1:
+						data_variant.asset = (self.PATTERN_FILE_TEXT_ASSET % (path_text, int(variant_index))).replace(self.PATH_PHASE_REMAKED, "remaked://")
+						data_variant.plain = (self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index))).replace(self.PATH_PHASE_REMAKED, "remaked://")
 
-					data_variant.links = ObjDict()
+						data_variant.links = ObjDict()
 
-					for link_index, link in variant.content.linktable_meta.iteritems():
-						linktable = variant.content.linktable[str(link_index)]
+						for link_index, link in variant.content.linktable_meta.iteritems():
+							linktable = variant.content.linktable[str(link_index)]
 
-						data_link = ObjDict()
-						data_link.area = ObjDict()
-						data_link.actions = ObjDict()
+							data_link = ObjDict()
+							data_link.area = ObjDict()
+							data_link.actions = ObjDict()
 
-						data_link.area.topleft_x = link.content.topleft_x
-						data_link.area.topleft_y = link.content.topleft_y
-						data_link.area.bottomright_x = link.content.bottomright_x
-						data_link.area.bottomright_y = link.content.bottomright_y
+							data_link.area.topleft_x = link.content.topleft_x
+							data_link.area.topleft_y = link.content.topleft_y
+							data_link.area.bottomright_x = link.content.bottomright_x
+							data_link.area.bottomright_y = link.content.bottomright_y
 
-						for action_index, action in linktable.content.pieces.iteritems():
-							data_action = ObjDict()
+							for action_index, action in linktable.content.pieces.iteritems():
+								data_action = ObjDict()
 
-							data_action.type = action.mode
-							data_action.params = ObjDict()
+								data_action.type = action.mode
+								data_action.params = ObjDict()
 
-							if data_action.type == 4:
-								data_action.params.content = action.data.textfile
-								data_action.params.area = ObjDict()
-								data_action.params.slider = ObjDict()
+								if data_action.type == 4:
+									data_action.params.content = action.data.textfile
+									data_action.params.area = ObjDict()
+									data_action.params.slider = ObjDict()
 
-								data_action.params.area.topleft_x = action.data.topleft_x
-								data_action.params.area.topleft_y = action.data.topleft_y
-								data_action.params.area.width = action.data.width
-								data_action.params.area.height = action.data.height
+									data_action.params.area.topleft_x = action.data.topleft_x
+									data_action.params.area.topleft_y = action.data.topleft_y
+									data_action.params.area.width = action.data.width
+									data_action.params.area.height = action.data.height
 
-								data_action.params.slider.topleft_x = action.data.slider_topleft_x
-								data_action.params.slider.topleft_y = action.data.slider_topleft_y
+									data_action.params.slider.topleft_x = action.data.slider_topleft_x
+									data_action.params.slider.topleft_y = action.data.slider_topleft_y
 
-							elif data_action.type == 6:
-								#data_linktable_content_piece.data.foo = linktable_content_piece.data.foo # TODO
-								data_action.params.foo = ""
+								elif data_action.type == 6:
+									#data_linktable_content_piece.data.foo = linktable_content_piece.data.foo # TODO
+									data_action.params.foo = ""
 
-							elif data_action.type == 9:
-								#data_linktable_content_piece.data.foo = linktable_content_piece.data.foo # TODO
-								data_action.params.foo = ""
+								elif data_action.type == 9:
+									#data_linktable_content_piece.data.foo = linktable_content_piece.data.foo # TODO
+									data_action.params.foo = ""
 
-							elif data_action.type == 11:
-								data_action.params.foo = action.data.foo
+								elif data_action.type == 11:
+									data_action.params.foo = action.data.foo
 
-							elif data_action.type == 12:
-								data_action.params.id = action.data.id
-								data_action.params.foo = action.data.foo
+								elif data_action.type == 12:
+									data_action.params.id = action.data.id
+									data_action.params.foo = action.data.foo
 
-							elif data_action.type == 13:
-								data_action.params.id = action.data.id
-								data_action.params.content = action.data.textfile
+								elif data_action.type == 13:
+									data_action.params.id = action.data.id
+									data_action.params.content = action.data.textfile
 
-							elif data_action.type == 14:
-								data_action.params.id = action.data.id
-								data_action.params.value = action.data.value
+								elif data_action.type == 14:
+									data_action.params.id = action.data.id
+									data_action.params.value = action.data.value
 
-							elif data_action.type == 20:
-								data_action.params.content = action.data.textfile
-								data_action.params.foo = action.data.foo
+								elif data_action.type == 20:
+									data_action.params.content = action.data.textfile
+									data_action.params.foo = action.data.foo
 
-							elif data_action.type == 65535:
-								data_action.params.foo = action.data.foo
+								elif data_action.type == 99:
+									data_action.params.length = action.data.length
+									#data_linktable_content_piece.data.foo = linktable_content_piece.data.foo # TODO
+									data_action.params.foo = ""
 
-							else:
-								print "Unknown action type: %s (text_index=%s, variant_index=%s, link_index=%s)" % (action.mode, text_index, variant_index, link_index)
-								sys.exit()
+								elif data_action.type == 65535:
+									data_action.params.foo = action.data.foo
 
-							data_link.actions[str(action_index)] = data_action
+								else:
+									print "Unknown action type: %s (text_index=%s, variant_index=%s, link_index=%s)" % (action.mode, text_index, variant_index, link_index)
+									sys.exit()
 
-						data_variant.links[str(link_index)] = data_link
+								data_link.actions[str(action_index)] = data_action
 
-					if self.source.version > 3:
-						with open(variant.content.title.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
-							variant_title = f.read()
+							data_variant.links[str(link_index)] = data_link
 
-						all_bytes = string.maketrans("", "")
-						variant_title = variant_title.translate(all_bytes, all_bytes[:32])
-						data_variant.title = ""
+						if self.source.version > 3:
+							with open(variant.content.title.replace("decompiled://", self.PATH_PHASE_DECOMPILED), "rb") as f:
+								variant_title = f.read()
 
-						for char_index, char in enumerate(variant_title):
-							if ord(char) < 128:
-								data_variant.title += char
-							else:
-								data_variant.title += self.CHARTABLE[ord(char) - 128]
+							all_bytes = string.maketrans("", "")
+							variant_title = variant_title.translate(all_bytes, all_bytes[:32])
+							data_variant.title = ""
+
+							for char_index, char in enumerate(variant_title):
+								if ord(char) < 128:
+									data_variant.title += char
+								else:
+									data_variant.title += self.CHARTABLE[ord(char) - 128]
+
+					else:
+						data_variant.plain = (self.PATTERN_FILE_TEXT_PLAIN % (path_text, int(variant_index))).replace(self.PATH_PHASE_REMAKED, "remaked://")
 
 					data_text.variants[str(variant_index)] = data_variant
 
